1dce9f0d3c2859b5085fafa02866ad04c7541709
jrppena <jppena@up.edu.ph>
2023-11-25 18:46:05 +0800

    added base code for loops

---
+++ subunits/loops.py
@@ -0,0 +1,14 @@
+
+class Loops():
+
+    def __init__(self, tab , pars) -> None:
+        self.tab = tab
+        self.pars = pars
+
+    def main(self):
+        print(f""" 
+            capture: {self.tab.capture}
+            capture group: {self.tab.capture_group}
+            line: {self.tab.line}
+            row: {self.tab.row}
+              """)
