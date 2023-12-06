ba711d8f768b658ddd5a9efb0137821de50e6eca
RaphaelVispo <rsvispo@up.edu.ph>
2023-12-07 00:22:02 +0800

    Feat: Add SMOOSH

--- subunits/output.py
+++ subunits/output.py
@@ -4,15 +4,19 @@ class Output():
     def __init__(self, tab , pars) -> None:
         self.tab = tab
         self.pars = pars
-        self.concat = ""
+        
 
     def main(self):
         self.concatination("^\+ ")
+        self.tab.terminal += self.tab.variables["IT"]
 
     def concatination(self, delimiter= "^AN "):
+
         # <output>::= VISIBLE <literal> <concat>
         # <concat> ::= <linebreak> | + <literal> <concat> 
-        self.tab.terminal += str(self.pars.get_lexemes(["literal"]))
+        self.concat = ""
+        self.concat += str(self.pars.get_lexemes(["boolean", "infinite","expression", "literal"]))
+
 
         while True:
             if self.pars.get_rid("^! ?","delimeter"):
@@ -23,7 +27,10 @@ class Output():
                 break
 
             self.pars.get_rid(delimiter,"delimeter", "There should be '+' to concatinate")
-            self.tab.terminal += str(self.pars.get_lexemes(["literal"])) 
+            self.concat += str(self.pars.get_lexemes(["boolean", "infinite","expression", "literal"]))
+
+
+        self.tab.variables["IT"] = self.concat
 
 
 
