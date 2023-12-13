673840a20ef8d65bb4a19b124e2a8b4f4d007203
RaphaelVispo <rsvispo@up.edu.ph>
2023-12-13 17:53:22 +0800

    Docs: input

--- subunits/input.py
+++ subunits/input.py
@@ -7,12 +7,22 @@ class Input():
         self.pars = pars
 
     def main(self):
+        # grammar for variable
         self.pars.get_rid("^([a-zA-Z][a-zA-Z0-9_]*) ?", "variable name", "there should be a variable")
-        s.Variable(self.tab, self.pars).get_var()
-        var = self.tab.capture_group[0]
+
+        # get variable with error handling
+        var = s.Variable(self.tab, self.pars).get_var()
+
+        # put the current data in the front end
         self.tab.show_data()
+
+        # wait for the users' input
         self.tab.root_front.wait_variable(self.tab.buffer)
+
+        # format the user's input
         self.tab.variables[var]  = self.tab.buffer.get().strip()
+
+        # grammar: new line
         self.pars.get_rid_new_line()
         return  self.tab.variables[var]
 
