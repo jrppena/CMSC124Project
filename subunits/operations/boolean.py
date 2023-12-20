bc6d6c15261953f7f3c819bef82d69eacf29c065
rebberchicken <brtandang2@up.edu.ph>
2023-12-20 13:03:20 +0800

    removed print statements

--- subunits/operations/boolean.py
+++ subunits/operations/boolean.py
@@ -17,31 +17,24 @@ class Boolean():
 
     def both_of(self):
         self.logic_op()
-        print(self.literal1,"and",self.literal2)
         self.result = self.literal1 and self.literal2
-        print(self.result)
         return self.result
 
     def either_of(self):
         self.logic_op()
-        print(self.literal1,"or",self.literal2)
         self.result = self.literal1 or self.literal2
-        print(self.result)
         return self.result
 
     def won_of(self):
         # <won_of> ::= WON OF <literal> AN <literal>
         self.logic_op()
-        print(self.literal1,"xor",self.literal2)
         self.result = (self.literal1 or self.literal2) and not (self.literal1 and self.literal2)
-        print("xor:",self.result)
         return self.result
 
     def not_operation(self):
         # <not_operation> ::= NOT <boolean> | NOT <literal>
         self.literal1 = self.pars.get_lexemes(["boolean", "literal"])
         self.result = not self.literal1
-        print("not:", self.result)
         return self.result
 
     def all_of(self):
@@ -58,8 +51,7 @@ class Boolean():
             self.literal1 = self.pars.get_lexemes(["boolean", "literal"])
             self.result = self.result and self.literal1
         
-        print("all of:", self.result)
-
+        return self.result
 
     def any_of(self):
         # <any_of> ::= ANY OF <boolean> | <literal> AN <boolean> | <literal> <loop> | MKAY
@@ -75,6 +67,6 @@ class Boolean():
             self.literal1 = self.pars.get_lexemes(["boolean", "literal"])
             self.result = self.result or self.literal1
         
-        print("all of:", self.result)
+        return self.result
 
    
