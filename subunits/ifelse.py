f3b02343f33f8a606fde86f6ef388dd7c0521ce9
rebberchicken <brtandang2@up.edu.ph>
2023-12-21 18:04:14 +0800

    chore: finished if else, wait for get rid spacing

--- subunits/ifelse.py
+++ subunits/ifelse.py
@@ -6,19 +6,32 @@ class IfElse():
 
     def main(self):
         self.pars.get_rid_new_line()
+        self.pars.get_rid("^ *", "spacing")
+        self.pars.get_rid("^YA RLY", "if statement", "There should be a 'YA RLY' if-statement")
+        self.pars.get_rid_new_line()
+        
+        delimiter = "^(NO WAI|MEBBE|OIC) ?"
+        has_run = False
+        captured_delm = "YA RLY"
+        while True:
+            if ((self.tab.variables["IT"] and not has_run) or (captured_delm == "NO WAI" and not has_run)): # YA RLY and MEBBE, and NO WAI
+                self.pars.run_lines(delimiter)
+                has_run = True
+            else: # skip
+                self.pars.run_lines(delimiter, skip=True)
 
-        if self.tab.variables["IT"]:
-            self.pars.run_lines("^NO WAI *")
-            self.pars.get_rid_new_line()
-            self.pars.run_lines("^OIC *", skip=True)
-            self.pars.get_rid_new_line()
-        else:
-            self.pars.run_lines("^NO WAI *", skip=True)
-            self.pars.get_rid_new_line()
-            self.pars.run_lines("^OIC *")
-            self.pars.get_rid_new_line()
+            captured_delm = self.tab.capture_group[0] # capture delimiter
+            
+            if captured_delm == "OIC": 
+                self.pars.get_rid_new_line()
+                break 
+            elif captured_delm == "NO WAI":
+                self.pars.get_rid_new_line()
+                delimiter = "^(OIC) ?"
+                continue
+            else: # MEBBE
+                self.pars.get_lexemes(["comparison"])
+                continue
 
     def skip(self):
         self.pars.run_lines("^OIC *", skip=True)
-
-
