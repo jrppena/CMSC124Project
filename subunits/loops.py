3a105cfd6273df799953abc2b1a07d9a28670917
jrppena <jppena@up.edu.ph>
2023-12-22 21:43:18 +0800

    feat: base for loops

--- subunits/loops.py
+++ subunits/loops.py
@@ -12,3 +12,23 @@ class Loops():
             line: {self.tab.line}
             row: {self.tab.row}
         """)
+
+        self.pars.get_rid("([a-zA-Z][a-zA-Z0-9_]*) ?", "label", "there should be a valid label")
+        label = self.tab.capture
+        print("label: ",label)
+
+        self.pars.get_rid("(UPPIN|NERFIN) ?", "label", "there should be a valid label")
+        operation = self.tab.capture
+        print("operation: ",operation)
+
+        self.pars.get_rid("YR ?", "delimeter", "there should be a YR delimeter")
+        self.pars.get_rid("([a-zA-Z][a-zA-Z0-9_]*) ?", "label", "there should be a valid label")
+        variable = self.tab.capture
+
+        self.pars.get_rid("(TIL|WILE) ?", "condition")
+        condition = self.tab.capture
+
+        print("condition: ", condition)
+        print(self.tab.line)
+        # self.tab.semantic_error("here")
+        var = self.pars.get_lexemes(["comparison"])
